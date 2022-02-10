0fab9fad3a5eba08039c1860691d98832a0c51ae
Furkan Berkay ÇAKMAK <furkanberkaycakmak@icloud.com>
2022-02-10 22:24:39 +0300

    Initial Commit

---
+++ VKProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKProject
+//
+//  Created by Berkay ÇAKMAK on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
