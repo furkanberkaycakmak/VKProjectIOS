5e3394a57544ef7edc01a5ee5a415082c3acc997
furkanberkaycakmak <furkanberkaycakmak@icloud.com>
2022-02-13 16:12:25 +0300

    ProductModel updated.

--- VKProject/Models/ProductModel.swift
+++ VKProject/Models/ProductModel.swift
@@ -7,12 +7,77 @@
 
 import Foundation
 
-class ProductModel: Codable {
+class GetProductModel: Codable {
+    let currency : String
+    let price : Double
     let id : String
     let name : String
     let description : String
-    let currency : String
-    let price : Double
     let imgUrl : String
   }
 
+class PostProductModel : Codable {
+    /* {
+      "id": "string",
+      "name": "string",
+      "description": "string",
+      "imgUrl": "string"
+    }     DOLDURULARAK YOLLANACAK
+     */
+    
+    let id : String
+    let name : String
+    let description : String
+    let imgUrl : String
+    let _id : String
+
+}
+
+class GetProductDetailsModel: Codable {
+    
+    /*
+     "id": String
+     
+     DOLDURULARAK YOLLANACAK
+     */
+    let currency : String
+    let price : Double
+    let id : String
+    let name : String
+    let description : String
+    let imgUrl : String
+    
+    
+    
+}
+
+
+class UpdateProductModel: Codable {
+    /*
+     "id": "string",
+     "name": "string",
+     "description": "string",
+     "imgUrl": "string"
+     DOLDURULARAK YOLLANACAK
+     */
+    let id : String
+    let name : String
+    let description : String
+    let imgUrl : String
+    
+}
+
+
+class DeleteProductModel: Codable {
+    /*
+     "id": String
+     
+     DOLDURULARAK YOLLANACAK
+     */
+    
+    let ok : Int
+}
+
+
+
+//TODO
